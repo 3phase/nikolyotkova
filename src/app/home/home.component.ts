4dce257a7909a663505853437e241ebe20ccf06c
3phase <pbozhinow@gmail.com>
2022-05-24 23:22:51 +0300

    Makes flickity shit load every time the component has been loaded

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  constructor() {
+    new Promise((resolve) => {
+      this.loadScript();
+      resolve(true);
+    });
+  }
 
-  ngOnInit(): void {
+  public loadScript() {
+    var scripts = document.getElementsByTagName("script");
+    for (var i = 0; i < scripts.length; ++i) {
+      if (scripts[i].getAttribute('src') !== null && scripts[i].getAttribute('src')?.includes("flickity.pkgd.min.js")) {
+        scripts[i].remove();
+      }
+    }
+
+    var dynamicScripts = ["https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"];
+
+    for (var i = 0; i < dynamicScripts.length; i++) {
+      let node = document.createElement('script');
+      node.src = dynamicScripts[i];
+      node.type = 'text/javascript';
+      node.async = false;
+      document.getElementsByTagName('head')[0].appendChild(node);
+    }
   }
 
 }
